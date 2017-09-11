f8502cb32e4747c5ee3aa237ea4fb25a9b886912
kapphire <wealthy.gurub@yahoo.com>
2017-09-11 22:29:32 +0200

    allowed host

--- nfx/urls.py
+++ nfx/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^navProved/', include('navProved.urls')),
+    url(r'^$', include('navProved.urls')),
 ]
