156aebcacd232ee2bb927ca30d0e243328624cc3
kapphire <wealthy.gurub@yahoo.com>
2017-09-12 20:49:26 +0200

    intermidiate

--- nfx/urls.py
+++ nfx/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'/', include('navProved.urls')),
+    url(r'^$', include('navProved.urls')),
     url(r'^navProved/', include('navProved.urls')),
 ]
