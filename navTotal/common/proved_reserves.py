7b67232662904594a05fed2293f0388f9c9e42a8
kapphire <ken.sapphire87@yahoo.com>
2017-10-10 21:44:01 +0200

    wow_fix2

--- navTotal/common/proved_reserves.py
+++ navTotal/common/proved_reserves.py
@@ -10,6 +10,7 @@ class ProvedReserves(object):
 	def get_proved_reserves(self):
 		proved_reserves_arr = ['United States', '']  # ======= Should be updated ===========
 		proved_reserves_dict = {}
+		ordered = {}
 		for individual in self.nav_proved:
 			if individual.name in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']:
 				proved_reserves_dict[int(individual.name)] = individual.value
